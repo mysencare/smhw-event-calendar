9cb3ad190d29603b660b3ee49dff1c62922e7120
smnkrt <szymon.krywult@netguru.pl>
2017-11-22 22:46:24 +0100

    add EventsController#create action with js partial

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,5 +4,18 @@ class EventsController < ApplicationController
     events = Event.all
     render :index, locals: { event: event, events: events }
   end
+
+  def create
+    event = Event.new(event_params)
+    event = event.save ? Event.new : event
+    render :create, locals: { event: event }
+  end
+
+  private
+
+  def event_params
+    params
+      .require(:event)
+      .permit(:name, :start_date, :end_date, :description)
   end
 end
