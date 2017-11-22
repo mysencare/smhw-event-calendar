3c828df99682b71e00bd2f93f37c90e6e2dcdcf2
smnkrt <szymon.krywult@netguru.pl>
2017-11-22 23:58:48 +0100

    refactor EventsController
    use Events::Creator in create action
    use Events::CurrentWeekFetcher to fetch Events within selected week

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,21 +1,28 @@
 class EventsController < ApplicationController
   def index
-    event  = Event.new
-    events = Event.all
+    event = Event.new
     render :index, locals: { event: event, events: events }
   end
 
   def create
-    event = Event.new(event_params)
-    event = event.save ? Event.new : event
-    render :create, locals: { event: event }
+    event = Events::Creator.new(event_params).call
+    render :create, locals: { event: event, events: events }
   end
 
   private
+
+  def events
+    Events::CurrentWeekFetcher.new(week_start_date).fetch
+  end
 
   def event_params
     params
       .require(:event)
       .permit(:name, :start_date, :end_date, :description)
+  end
+
+  def week_start_date
+    start_date = params[:start_date]
+    start_date.present? ? Date.parse(start_date) : Date.current
   end
 end
